5c4d253ea2a973383de4eed883975acf7eab4042
Phlippe Maradan <pmaradan@gmail.com>
2020-01-26 15:58:05 +0100

    Initial commit

---
+++ resources/js/bookables/BookableListItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card mb-2 w-100">
+        <div class="card-body">
+            <router-link :to="{ name: 'bookable', params: { id } }">
+                <h5>{{ title }}</h5>
+            </router-link>
+            <p class="card-text">{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {title: String, description: String, id: Number},
+}
+</script>
