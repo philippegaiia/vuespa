5edc166d33f6b6949a51c3032a7cd55c833a4185
Phlippe Maradan <pmaradan@gmail.com>
2020-01-28 22:29:46 +0100

    Lecture 77 end

---
+++ resources/js/review/Review.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      <div v-if="alreadyReviewed">
+        <h3>You 've already left a review for this booking!</h3>
+      </div>
+      <div v-else>
+        <div class="form-group">
+          <label class="text-muted">Select the star rating (1 to 5)</label>
+          <star-rating class="fa-3x" v-model="review.rating"></star-rating>
+        </div>
+        <div class="form-group">
+          <label for="content" class="text-muted">Describe your experience with</label>
+          <textarea
+            name="content"
+            id
+            cols="30"
+            rows="10"
+            class="form-control"
+            v-model="review.content"
+          ></textarea>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block">Submit</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      review: {
+        rating: 5,
+        content: null
+      },
+      existingReview: null,
+      loading: false
+    };
+  },
+  created() {
+    this.loading = true;
+    //1. if review already exists (in review table by id)
+    axios
+      .get(`/api/reviews/${this.$route.params.id}`)
+      .then(response => (this.existingReview = response.data.data))
+      .catch(err => {})
+      .then(() => (this.loading = false));
+    //2. fetch a booking by a review key
+    //3. Store th review
+  },
+  computed: {
+    alreadyReviewed() {
+      return this.existingReview !== null;
+    }
+  }
+};
+</script>
