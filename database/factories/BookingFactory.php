e964001287f97e12c3fac1d02c9955e1e3228a1c
Phlippe Maradan <pmaradan@gmail.com>
2020-01-29 20:33:15 +0100

    Lecture 79 end

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -7,11 +7,11 @@ use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 $factory->define(Booking::class, function (Faker $faker) {
-        $from = Carbon::instance($faker->dateTimeBetween('-1 month', '+1 month'));
-        $to = (clone $from)->addDays(random_int(0, 14));
+    $from = Carbon::instance($faker->dateTimeBetween('-1 month', '+1 month'));
+    $to = (clone $from)->addDays(random_int(0, 14));
 
-        return [
-            'from' => $from,
-            'to' => $to
-        ];
+    return [
+        'from' => $from,
+        'to' => $to,
+    ];
 });
--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,6 @@ Route::apiResource('bookables', 'Api\BookableController')->only(['index', 'show'
 Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityController')->name('bookables.availability.show');
 Route::get('bookables/{bookable}/reviews', 'Api\BookableReviewController')->name('bookable.reviews.index');
 
-Route::apiResource('reviews', 'Api\ReviewCOntroller')->only(['show']);
+Route::get('/booking-by-review/{reviewKey}', 'Api\BookingByReviewController')->name('booking-by-review.show');
+
+Route::apiResource('reviews', 'Api\ReviewController')->only(['show']);
